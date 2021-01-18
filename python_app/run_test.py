d7d8283792d771be382796421731566f05a4bf3e
paula-anna <72548408+paula-anna@users.noreply.github.com>
2021-01-18 15:06:07 +0100

    fix and test gender_function plurals

---
+++ python_app/run_test.py
@@ -0,0 +1,8 @@
+from test.test_gender_function import test_gender_function
+
+
+print('\n\n\nrunning test')
+
+test_gender_function()
+
+print('...everything okay\n\n\n')
---
+++ python_app/test/test_gender_function.py
@@ -0,0 +1,19 @@
+from test.util import assert_equal
+from text_converter.gender_function import transform_text
+
+
+def test_gender_function():
+    test_cases = [
+        ['gods', 'goddesses'],
+        ['Gods', 'Goddesses'],
+        ['ladies', 'gentlemen'],
+        ['father-in-law', 'mother-in-law'],
+        ['conductors', 'conductresses'],
+        ['his', 'her'],
+        ['My father said he was a superwomen.', 
+         'My mother said she was a supermen.'], 
+        ['dudes', 'ladies']
+    ]
+
+    for inp, expectation in test_cases: 
+        assert_equal(transform_text(inp), expectation)
